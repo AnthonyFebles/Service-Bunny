2d42c5cb2172cc939bacc43a9146aba536bd3678
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-11 11:57:58 -0500

    first commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
@@ -14,20 +14,16 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
-  );
+		<>
+			<Navigation isLoaded={isLoaded} />
+			{isLoaded && (
+				<Routes>
+					<Route path="/login" element={<LoginFormPage />}></Route>
+					<Route path="/signup" element={<SignupFormPage />}></Route>
+				</Routes>
+			)}
+		</>
+	);
 }
 
 export default App;
