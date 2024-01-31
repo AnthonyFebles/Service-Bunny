dd9ad97bdb44b32d909f3f6bd26ea013d96f31b1
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-30 19:54:04 -0500

    sign_up form and edits to models lengths to make more sense

--- app/forms/bookings_form.py
+++ app/forms/bookings_form.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class BookingForm(FlaskForm):
-    scheduled_start = DateTimeField(
-        "When", validators=[DataRequired()], format="%Y-%m-%d %H:%M:%S")
-    
+    scheduled_start = DateTimeField("When", validators=[DataRequired()], format="%Y-%m-%d %H:%M:%S")
+    scheduled_end = DateTimeField("Estimated End", format="%Y-%m-%d %H:%M:%S")
+    started_at = DateTimeField("Actual Start", format="%Y-%m-%d %H:%M:%S")
+    stopped_at = DateTimeField("Actual End", format="%Y-%m-%d %H:%M:%S")
--- app/forms/jobs_form.py
+++ app/forms/jobs_form.py
@@ -4,9 +4,8 @@ from wtforms.validators import DataRequired, Length
 
 
 class JobForm(FlaskForm):
-    worker_id = IntegerField(
-        "Worker Id")
+    worker_id = IntegerField("Worker Id")
     description = StringField("Description", validators=[DataRequired(), Length(max=500)])
     solution = StringField("Solution", validators=[Length(max=500)])
-    customer_check = BooleanField("Customer Check ")
-    employee_check = BooleanField("Employee Check ")
+    customer_check = BooleanField("Customer Check")
+    employee_check = BooleanField("Employee Check")
