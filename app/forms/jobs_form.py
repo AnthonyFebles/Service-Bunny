0ae8c38a544ec4f93d833e13f72a42cc296f74fc
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-30 19:15:14 -0500

    jobs form

---
+++ app/forms/jobs_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, BooleanField
+from wtforms.validators import DataRequired, Length
+
+
+class JobForm(FlaskForm):
+    worker_id = IntegerField(
+        "Worker Id")
+    description = StringField("Description", validators=[DataRequired(), Length(max=500)])
+    solution = StringField("Solution", validators=[Length(max=500)])
+    customer_check = BooleanField("Customer Check ")
+    employee_check = BooleanField("Employee Check ")
