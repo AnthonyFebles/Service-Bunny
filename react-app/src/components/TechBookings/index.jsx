245d7ded5de804c89d371c1c9c961d3271ba7a34
AnthonyFebles <anthonyfebles21@outlook.com>
2024-02-06 16:38:48 -0500

    Tech can start and end a job, which will disable the appropriate buttons and will say waiting for custoimer approval when a job is done

--- react-app/src/components/TechBookings/index.jsx
+++ react-app/src/components/TechBookings/index.jsx
@@ -5,12 +5,13 @@ import { getBookings, updateBooking } from "../../store/bookings";
 import { getBooking } from "../../store/booking";
 import { getOne } from "../../store/jobDetails";
 
-const TechBookings = ({booking}) => {
+const TechBookings = ({ booking }) => {
 	const dispatch = useDispatch();
 
-    
-
 	const [isLoading, setIsLoading] = useState(true);
+	const [isStarted, setIsStarted] = useState(false);
+	const [isDone, setIsDone] = useState(false);
+	const [customerApproval, setCustomerApproval] = useState("");
 
 	const sessionUser = useSelector((state) => state.session.user);
 	const navigate = useNavigate();
@@ -20,17 +21,26 @@ const TechBookings = ({booking}) => {
 		// .bookings.map((bookingId) => state.bookings[bookingId]);
 	});
 
-    const thisBooking = useSelector((state) => {
-        return state.booking
-    })
+	const thisBooking = useSelector((state) => {
+		return state.booking;
+	});
 
-    console.log(thisBooking, "this booking" )
-    console.log(booking.id, "booking prop")
+	console.log(thisBooking, "this booking");
+	console.log(booking.id, "booking prop");
 
 	useEffect(() => {
-			dispatch(getBookings())
-            dispatch(getBooking(booking.id))
-			.then(() => setIsLoading(false));
+		dispatch(getBookings());
+		dispatch(getBooking(booking.id)).then(() => setIsLoading(false));
+
+		if (booking.started_at) {
+			setIsStarted(true);
+		}
+		if (booking.stopped_at) {
+			setIsDone(true);
+		}
+		if (booking.stopped_at && booking.started_at) {
+			setCustomerApproval("Awaiting Customer Approval");
+		}
 	}, [dispatch]);
 
 	// console.log(currJobs, "curr Jobs")
@@ -39,31 +49,66 @@ const TechBookings = ({booking}) => {
 
 	if (isLoading) return <>Loading...</>;
 
-    const handleStart = async () =>  {
-        const currDate = new Date()
-        const payload = {
-					id: booking.id,
-                    job_id: booking.job_id,
-                    user_id: sessionUser.id,
-					started_at: `${currDate.getFullYear()}-${currDate.getMonth()}-${currDate.getDate()} ${currDate.getHours()}:${currDate.getMinutes()}:${currDate.getSeconds()}`,
-				};
-    try{
-        await dispatch(updateBooking(payload))
-        dispatch(getBooking(booking.id))
-        dispatch(getOne(booking.job_id))
-    }catch(error){
-        console.log(error)
-    }
-    }
+	const handleStart = async () => {
+		const currDate = new Date();
+		const payload = {
+			id: booking.id,
+			job_id: booking.job_id,
+			user_id: sessionUser.id,
+			started_at: `${currDate.getFullYear()}-${currDate.getMonth()}-${currDate.getDate()} ${currDate.getHours()}:${currDate.getMinutes()}:${currDate.getSeconds()}`,
+		};
+		try {
+			await dispatch(updateBooking(payload))
+				.then(() => dispatch(getBooking(booking.id)))
+				.then(() => dispatch(getOne(booking.job_id)));
+			setIsStarted(true);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleEnd = async () => {
+		const currDate = new Date();
+		const payload = {
+			id: booking.id,
+			job_id: booking.job_id,
+			user_id: sessionUser.id,
+			stopped_at: `${currDate.getFullYear()}-${currDate.getMonth()}-${currDate.getDate()} ${currDate.getHours()}:${currDate.getMinutes()}:${currDate.getSeconds()}`,
+		};
+		try {
+			await dispatch(updateBooking(payload))
+				.then(() => dispatch(getBooking(booking.id)))
+				.then(() => dispatch(getOne(booking.job_id)));
+			setIsDone(true);
+            setCustomerApproval("Awaiting Customer Approval");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div className="manager_container">
 			<div>Actually Started On : {booking.started_at}</div>
 			<div>Completed On : {booking.stopped_at}</div>
 			<div className="job_details-button_container">
-				<button className="job_details-start_button" onClick={handleStart}>Start Job</button>
-				<button className="job_details-end_button">End Job</button>
-				<button className="job_details-cancel_button">Cancel Job</button>
+				<button
+					className="job_details-start_button"
+					disabled={isStarted}
+					onClick={handleStart}
+				>
+					Start Job
+				</button>
+				<button
+					className="job_details-end_button"
+					disabled={isDone}
+					onClick={handleEnd}
+				>
+					End Job
+				</button>
+				<button className="job_details-cancel_button" disabled={isStarted}>
+					Cancel Job
+				</button>
+				<p>{customerApproval}</p>
 			</div>
 		</div>
 	);
